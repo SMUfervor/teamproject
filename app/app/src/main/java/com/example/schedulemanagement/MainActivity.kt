cacb2887e2dc0f6b6e727034b837c116b3df92de
oldJinGyu <chwlsrb00225@naver.com>
2023-06-07 17:01:43 +0900

    app

--- app/app/src/main/java/com/example/schedulemanagement/MainActivity.kt
+++ app/app/src/main/java/com/example/schedulemanagement/MainActivity.kt
@@ -71,7 +71,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             builder.setNegativeButton("취소") { dialog, which ->
-                Toast.makeText(this, "목록 추가가 취소되었습니다.", Toast.LENGTH_SHORT).show()
+
             }
 
             val dialog = builder.create()
